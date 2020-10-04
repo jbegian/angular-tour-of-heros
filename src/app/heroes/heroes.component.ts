2ed11178f11737295cd8698cf0ca7daba157bf2b
Jordan Begian <begian.j@gmail.com>
2020-10-04 17:06:22 -0400

    add getHeroes method

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +10,9 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
 
@@ -19,6 +20,11 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   ngOnInit(): void {
+    this.getHeroes();
   }
 }
