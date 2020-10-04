e66f45842c7b1dc12be17815e1e4d9a9589f6d4f
Jordan Begian <begian.j@gmail.com>
2020-10-04 16:55:52 -0400

    add getHeros method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,7 @@ export class HeroService {
 
   constructor() { }
 
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
